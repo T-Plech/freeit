7e6d9b144c50b817dbd892b23821844acf360612
T-Plech <tricsi_k@mail.ru>
2021-05-24 15:34:56 +0300

    Zadanie24 from Java_Begginer_chapter2.

---
+++ lesson5/src/RunnerTime.java
@@ -0,0 +1,18 @@
+
+
+public class RunnerTime {
+    public static void main(String[] args) {
+        Time t1 = new Time(5,10,2);
+        Time t2 = new Time(635890);
+
+
+        System.out.println(t1.toString());
+        System.out.println(t2.toString());
+        System.out.println(t1.allsec());
+        System.out.println(t2.allsec());
+        System.out.println(t1.compare_time(t2));
+
+
+    }
+
+}
---
+++ lesson5/src/Time.java
@@ -0,0 +1,50 @@
+//Создать класс и объекты, описывающие промежуток времени. +
+//        Сам промежуток должен задаваться 3 свойствами: секундами, минутами, часами. +
+//        Сделать методы
+//        * для получения полного количества секунд в объекте, +
+//        * сравнения двух объектов(метод должен работать аналогично compareTo в строках).+
+//        Создать два конструктора:
+//        *получающий общее количество секунд +
+//        * часы, минуты и секунды по отдельности +
+//        Сделать метод для вывода данных+
+
+public class Time {
+
+
+    private long sec;
+    private int min;
+    private int hours;
+
+
+    public Time(long sec, int min, int hours){
+        this.hours=hours;
+        this.min=min;
+        this.sec=sec;
+    }
+
+    public Time(long sec){
+        this.hours = (int) (sec/3600);
+        this.min = (int)((sec - this.hours*3600)/60);
+        this.sec = sec - this.hours*3600 -this.min*60;
+    }
+
+    public long allsec(){
+        long l = sec + min*60 + hours*3600;
+        return l;
+    }
+
+    public long compare_time(Time t1){
+
+        return  (this.allsec()-t1.allsec());
+
+    }
+
+    @Override
+    public String toString() {
+        return "Time{" +
+                "sec=" + sec +
+                ", min=" + min +
+                ", hours=" + hours +
+                '}';
+    }
+}
