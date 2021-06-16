bede17b572a23809e5456386caaa251308b4005f
T-Plech <79914819+T-Plech@users.noreply.github.com>
2021-06-16 14:10:18 +0200

    Lesson5 (#1)
    * Zadanie24 from Java_Begginer_chapter2.
    
    * Zadanie24 from Java_Begginer_chapter2.
    
    * Zadanie24 from Java_Begginer_chapter2. Added get's and set's
    
    * Zadanie25 from Java_Begginer_chapter2. Bankomat
    
    * change gitignore
    
    * Time constructor corrected
    
    * Zadanie25 Bankomat. Braces deleted on str.26. Deleted setting "operation" str.61
    
    * Zadanie26.Home Appliance. Refrigerators.
    
    * Zadanie24.Constructor str 20.
    
    * Zadanie25.Bankomat. str60. Condition if.
    
    * Zadanie24.Time. str20-22. konstructor
    
    * Zadanie24.Time. str20-22. konstructor

---
+++ lesson5/src/Zadanie26_BitovayaTehnika/Appliances.java
@@ -0,0 +1,9 @@
+package Zadanie26_BitovayaTehnika;
+
+public interface Appliances {
+    void powerOn();
+    void powerOff();
+    void inStain();
+    void offStain();
+    int power_consumption = 220;
+    }
