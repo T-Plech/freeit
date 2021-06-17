d2eff58bd36a3d0ca7bfe3b6955575f955cc54c7
T-Plech <79914819+T-Plech@users.noreply.github.com>
2021-06-17 13:07:53 +0200

    Delete lesson5/src/Zadanie26_BitovayaTehnika directory
    deleted unverified BitovayaTehnika. Create new in new branch

--- lesson5/src/Zadanie26_BitovayaTehnika/Large_home_appliances.java
+++
@@ -1,9 +0,0 @@
-package Zadanie26_BitovayaTehnika;
-
-public abstract class Large_home_appliances implements Appliances {
-
-    public void inStain(){System.out.println("Прибор подключен к розетке");};
-    public void offStain(){System.out.println("Прибор отключен от розетки");};
-
-
-}
--- lesson5/src/Zadanie26_BitovayaTehnika/Small_household_appliances.java
+++
@@ -1,9 +0,0 @@
-package Zadanie26_BitovayaTehnika;
-
-public abstract class Small_household_appliances implements Appliances {
-
-    boolean isStain;
-
-
-
-}
