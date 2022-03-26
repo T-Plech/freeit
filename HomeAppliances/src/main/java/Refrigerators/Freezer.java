2707e79daf50c938573326d0685f71ae6f313d75
T-plech <tricsi_k@mail.ru>
2022-03-26 11:44:49 +0300

    deleted HomeAppliance from branch dz5Computer

--- HomeAppliances/src/main/java/Refrigerators/Freezer.java
+++
@@ -1,9 +0,0 @@
-package Refrigerators;
-
-import HomeAppliances.HomeAppliances;
-
-public interface Freezer extends HomeAppliances {
-    void makeIce();
-    void freeze();
-    int freezerDegree = -18;
-}
